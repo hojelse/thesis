8093fe6252be903394589ef51ce2b121e7716689
Kristoffer Højelse <34659757+hojelse@users.noreply.github.com>
2024-02-27 15:42:48 +0100

    Add test.py

---
+++ test.py
@@ -0,0 +1,16 @@
+import subprocess
+counts = {
+	# 4: 1,
+	# 6: 1,
+	# 8: 3,
+	# 10: 9,
+	# 12: 38, # Errors on 22
+	# 14: 178, # Errors on 85,89,91,98,102,113
+	16: 1041
+}
+for n,count in counts.items():
+	for i in range(1, count+1):
+		file_name = f'graphs/{n}/{n}-{i}.bin'
+		print(file_name)
+		with open(file_name, 'rb') as f:
+			subprocess.run(['python3', 'count_hamcyc_faster.py'], stdin=f, text=True)
