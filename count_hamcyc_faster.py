4770d7fdf329dbf937c10baf1f180eaec2710aa6
Kristoffer Højelse <34659757+hojelse@users.noreply.github.com>
2024-02-28 14:15:16 +0100

    Refactor for readability

--- count_hamcyc_faster.py
+++ count_hamcyc_faster.py
@@ -16,90 +16,100 @@ def parse_graph() -> dict[int, list[int]]:
 		adj[i].append(x)
 	return adj
 
-def count_hamcyc_faster():
-	def find_faces(adj: dict[int, list[int]]) -> list[list[(int, int)]]:
-		# assume planar graph
-		# assume clockwise or counterclockwise ordering
-		edges = set([(i, j) for i in adj for j in adj[i]])
-		faces = []
-		while len(edges) > 0:
-			first_edge = edges.pop()
-			v = first_edge[1]
-			face = [first_edge]
-			next_edge = first_edge
-			while True:
-				u = adj[next_edge[1]][(adj[next_edge[1]].index(next_edge[0])+1)%3]
-				next_edge = (v, u)
-				if next_edge == first_edge:
-					faces.append(face)
-					break
-				edges.remove(next_edge)
-				face.append(next_edge)
-				v = u
-		return faces
+def find_faces(adj: dict[int, list[int]]) -> list[list[(int, int)]]:
+	# assume planar graph
+	# assume clockwise or counterclockwise ordering
+	edges = set([(i, j) for i in adj for j in adj[i]])
+	faces = []
+	while len(edges) > 0:
+		first_edge = edges.pop()
+		v = first_edge[1]
+		face = [first_edge]
+		next_edge = first_edge
+		while True:
+			u = adj[next_edge[1]][(adj[next_edge[1]].index(next_edge[0])+1)%3]
+			next_edge = (v, u)
+			if next_edge == first_edge:
+				faces.append(face)
+				break
+			edges.remove(next_edge)
+			face.append(next_edge)
+			v = u
+	return faces
 
-	adj = parse_graph()
-	N = len(adj)
+# def augment_graph(adj: dict[int, list[int]], non_special_faces: list[list[(int, int)]]) -> dict[int, list[int]]:
+# 	# Deep copy
+# 	aug_adj = dict([(k, [x for x in v]) for k,v in adj.items()])
+	
+# 	face_vertices = [chr(i+ord('A')) for i in range(len(non_special_faces))]
 
-	# augment the graph with face vertices
+# 	# add face vertices
+# 	for i,face in enumerate(non_special_faces):
+# 		x = face_vertices[i]
+# 		aug_adj[x] = [a for (a, b) in face]
+# 		for (a, b) in face:
+# 			aug_adj[a].insert((aug_adj[a].index(b)+1)%3, x)
+
+# 	# triangles
+# 	# triangles = sorted(set([(i, *sorted([j, k])) for i in face_vertices for j in aug_adj[i] for k in aug_adj[j] if i in aug_adj[k]]))
+
+# 	return aug_adj
+
+def flatten(xss):
+	return [x for xs in xss for x in xs]
+
+def count_hamiltonian_cycles(adj: dict[int, list[int]]) -> int:
+
+	def count_coverings(covered_vertices, non_special_faces) -> int:
+		covered_to_count = {(): 1}
+
+		def count_coverings_aux(i, covered_vertices) -> int:
+			covered_vertices = tuple(sorted(covered_vertices))
+			if covered_vertices in covered_to_count:
+				return covered_to_count[covered_vertices]
+
+			face = non_special_faces[i]
+
+			legal_edges = (
+				list(filter(lambda edge: edge[0] in covered_vertices and edge[1] in covered_vertices, face))
+				if i < len(non_special_faces)
+				else []
+			)
+
+			coverings_count = (
+				sum([count_coverings_aux(i+1, tuple(set(covered_vertices).difference({u, v}))) for (u, v) in legal_edges])
+				if len(legal_edges) > 0
+				else 0
+			)
+
+			covered_to_count[covered_vertices] = coverings_count
+			return coverings_count
+
+		return count_coverings_aux(0, covered_vertices)
+	
+	vertex_set = set(adj.keys())
+	faces = find_faces(adj)
+
+	# fix an edge - assumes vertex 1 is in the graph
+	fixed_edge = (1, adj[1][0])
+
+	# filter out special faces
 	def is_special_face(face):
 		return fixed_edge in face or tuple(reversed(fixed_edge)) in face
 
-	faces = find_faces(adj)
+	_non_special_faces = list(filter(lambda face: not is_special_face(face), faces))
 
-	# fix an edge
-	fixed_edge = (1, adj[1][0])
-
-	# Augmented graph - deep copy
-	aug_adj = dict([(k, [x for x in v]) for k,v in adj.items()])
-
-
-	# filter out special faces
-	non_special_faces = list(filter(lambda face: not is_special_face(face), faces))
-
-	face_vertices = [chr(i+ord('A')) for i in range(len(non_special_faces))]
-
-	# add face vertices
-	for i,face in enumerate(non_special_faces):
-		x = face_vertices[i]
-		aug_adj[x] = [a for (a, b) in face]
-		for (a, b) in face:
-			aug_adj[a].insert((aug_adj[a].index(b)+1)%3, x)
-
-	# triangles
-	triangles = sorted(set([(i, *sorted([j, k])) for i in face_vertices for j in aug_adj[i] for k in aug_adj[j] if i in aug_adj[k]]))
-
-	covered_to_count = {(): 1}
-	def count_ham_cycles(i=0, covered=([x for x in range(1, N+1)])) -> int:
-		covered = tuple(sorted(covered))
-		if covered in covered_to_count:
-			return covered_to_count[covered]
-
-		legal_edges = []
-		if i < len(non_special_faces):
-			legal_edges = list(filter(lambda p: p[0] in covered and p[1] in covered, non_special_faces[i]))
-
-		count = 0
-		if len(legal_edges) > 0:
-			count = sum([count_ham_cycles(i+1, tuple(set(covered).difference({a, b}))) for (a, b) in legal_edges])
-
-		covered_to_count[covered] = count
-		return count
-
-	def flatten(xss):
-		return [x for xs in xss for x in xs]
-
-	count_through_fixed_edge = count_ham_cycles()
-	# for k,v in covered_to_count.items():
-	# 	print(k, v)
+	# count the number of hamiltonian cycles through the fixed edge
+	count_through_fixed_edge = count_coverings(
+		covered_vertices = vertex_set,
+		non_special_faces = _non_special_faces
+	)
 
 	# count the number of hamiltonian cycles not through the fixed edge
-	covered_to_count = {(): 1}
-	non_special_faces = list(filter(lambda face: fixed_edge[0] not in flatten(face), non_special_faces))
-	count_not_through_fixed_edge = count_ham_cycles(covered=[x for x in range(1, N+1) if x not in fixed_edge])
-
-	# print(f"through_fixed: {count_through_fixed_edge}, not_through_fixed: {count_not_through_fixed_edge}")
-	# print(f"fixed edge: {fixed_edge}")
+	count_not_through_fixed_edge = count_coverings(
+		covered_vertices = set(filter(lambda v: v not in fixed_edge, vertex_set)),
+		non_special_faces = list(filter(lambda face: fixed_edge[0] not in flatten(face), _non_special_faces))
+	)
 
 	# render graph
 	# G = nx.DiGraph(adj)
@@ -107,4 +117,5 @@ def count_hamcyc_faster():
 	# plt.show()
 	return count_through_fixed_edge + count_not_through_fixed_edge
 
-print(count_hamcyc_faster())
+adj = parse_graph()
+print(count_hamiltonian_cycles(adj))
