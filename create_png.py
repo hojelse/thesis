60ba03f3f2a945d14e6295f8bff58de6702facbb
Kristoffer Højelse <34659757+hojelse@users.noreply.github.com>
2024-03-12 14:46:50 +0100

    Choose layout dependent on is/not planar

--- create_png.py
+++ create_png.py
@@ -6,6 +6,9 @@ from parse_graph import parse_graph_to_nx
 G = parse_graph_to_nx()
 
 plt.figure()
-nx.draw(G, pos=nx.planar_layout(G), with_labels=True)
+if nx.is_planar(G):
+	nx.draw(G, pos=nx.planar_layout(G), with_labels=True)
+else:
+	nx.draw(G, with_labels=True)
 plt.savefig(sys.stdout.buffer)
 # plt.show()
