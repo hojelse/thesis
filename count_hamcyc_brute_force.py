789f930bdb0457ff50c3e3d02dc62660f01313e0
Kristoffer Højelse <34659757+hojelse@users.noreply.github.com>
2024-04-26 01:07:53 +0200

    Refactor count_hamcyc_brute_force.py

--- count_hamcyc_brute_force.py
+++ count_hamcyc_brute_force.py
@@ -1,41 +1,24 @@
-import networkx as nx
 import itertools
-from parse_graph import parse_graph_to_nx
+from parse_graph import parse_graph_to_adj
 
-G = parse_graph_to_nx()
+G: dict[int, list[int]] = parse_graph_to_adj()
+vertex_set = G.keys()
+N = len(vertex_set)
 
-N = len(G.nodes)
-vertex_set = list(G.nodes)
-
-def cycle_exists(cycle):
-	for i in range(len(cycle)-1):
-		if cycle[i] in G[cycle[i+1]]:
-			pass
-		else:
+def valid(cycle: list[int]) -> bool:
+	for i in range(0, N-1):
+		if not cycle[i+1] in G[cycle[i]]:
 			return False
 	if not cycle[0] in G[cycle[-1]]:
 		return False
 	return True
 
-def count_ham_cyc():
-	ham_cycles = []
-	cycles = list(itertools.permutations(vertex_set))
-	finger_prints = set()
+def count_ham_cyc() -> int:
+	cycles = itertools.permutations(vertex_set)
 	count = 0
 	for cycle in cycles:
-		if cycle_exists(cycle):
+		if valid(cycle):
 			count += 1
-
-			finger_print = str(sorted(map(sorted, zip(cycle, list(cycle[1:]) + [cycle[0]]))))
-			if not finger_print in finger_prints:
-				ham_cycles.append(cycle)
-			finger_prints.add(finger_print)
-
-	return count//N//2
-	# print(f"count: {count}")
-	# print(f"unique: {count//N//2}")
-	# for ham_cyc in ham_cycles:
-	# 	print(ham_cyc)
+	return count//(2*N)
 
 print(count_ham_cyc())
-
