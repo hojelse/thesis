5cc80f7979ba74d08f02a453a4a92c8d2228970a
Kristoffer Højelse <34659757+hojelse@users.noreply.github.com>
2024-05-01 15:28:07 +0200

    Make ratcatcher and dual support multiple edges

--- code/carving_width_rat_catching.py
+++ code/carving_width_rat_catching.py
@@ -1,44 +1,42 @@
-from parse_graph import parse_graph_to_adj, adj_to_bytes
-from dual_graph import dual_graph, find_faces
+from parse_graph import parse_text_to_adj
+from dual_graph import dual_graph
 import math
 
 def carving_width_rat_catching(G) -> int:
-	dual, edge_to_link, link_to_edges, node_to_face, face_to_node, edge_to_node = dual_graph(G)
+	D, _D, linkid_to_nodepair, edgeid_to_linkid, linkid_to_edgeid, nodeid_to_edgeids, edgeid_to_nodeid, edgeid_to_vertexpair,_,_ = dual_graph(G)
+
+
 	# When the rat-catcher is on edge e, edge f is noisy iff there is
 	# a closed walk of at most length k containing e* and f* in G* .
 	# Return the un-noisy subgraph.
-	def noisy_links(l: tuple[int, int], k: int) -> set[tuple[int, int]]:
-		s,t = l
+	def noisy_links(l: int, k: int) -> set[int]:
+		s,t = linkid_to_nodepair[l]
 		noisy = set()
-		def bfs(walk_acc: list[tuple[int, int]]):
+		def bfs(walk_acc: list[int]):
 			if len(walk_acc) == k: return
-			v = walk_acc[-1][1]
-			if v == t:
-				noisy.update([tuple(sorted(e)) for e in walk_acc])
-			for u in dual[v]:
-				bfs(walk_acc + [(v, u)])
-		bfs([(t, s)])
-		return noisy
+			v = linkid_to_nodepair[walk_acc[-1]][1]
+			if v == s:
+				noisy.update(walk_acc)
+			for e in _D[v]:
+				bfs(walk_acc + [e])
+		bfs([l])
+		return set([abs(e) for e in noisy])
 
-	def quiet_links(l: tuple[int, int], k: int) -> set[tuple[int, int]]:
-		links = [tuple(sorted((x,y))) for (x,ys) in dual.items() for y in ys]
-		return set(links) - noisy_links(l, k)
+	def quiet_links(l: int, k: int) -> set[int]:
+		links = set([abs(e) for e in linkid_to_nodepair.keys()])
+		return links - noisy_links(l, k)
 
-	def quiet_edges(e: tuple[int, int], k: int) -> set[tuple[int, int]]:
-		quiet = set()
-		l = edge_to_link[tuple(sorted(e))]
-		for link in quiet_links(l, k):
-			for edge in link_to_edges[link]:
-				quiet.add(tuple(sorted(edge)))
-		return quiet
+	def quiet_edges(e: int, k: int) -> set[int]:
+		return set([abs(linkid_to_edgeid[l]) for l in quiet_links(edgeid_to_linkid[e], k)])
 
-	def quiet_components(e: tuple[int, int], k: int) -> dict[int, list[int]]:
+	def quiet_components(e: int, k: int) -> dict[int, list[int]]:
 		edges = quiet_edges(e, k)
 
 		quiet_subgraph = {v: [] for v in G.keys()}
-		for (u, v) in edges:
-			quiet_subgraph[u].append(v)
-			quiet_subgraph[v].append(u)
+		for e in edges:
+			u,v = edgeid_to_vertexpair[e]
+			quiet_subgraph[u].append(e)
+			quiet_subgraph[v].append(-e)
 
 		components = []
 		unseen = set(quiet_subgraph.keys())
@@ -49,11 +47,12 @@ def carving_width_rat_catching(G) -> int:
 			stack = [v]
 			while len(stack) > 0:
 				v = stack.pop()
-				for u in quiet_subgraph[v]:
-					if u in unseen:
-						unseen.remove(u)
-						stack.append(u)
-						component.append(u)
+				for e in quiet_subgraph[v]:
+					u,v = edgeid_to_vertexpair[e]
+					if v in unseen:
+						unseen.remove(v)
+						stack.append(v)
+						component.append(v)
 			components.append(component)
 
 		return components
@@ -72,24 +71,16 @@ def carving_width_rat_catching(G) -> int:
 		if max([len(G[v]) for v in G]) >= k:
 			return True
 
-		# print("k is", k)
-		edge_set = set([tuple(sorted((i, j))) for i in G for j in G[i]])
+		edge_set = edgeid_to_linkid.keys()
 
 		Te = set([(e, tuple(C)) for e in edge_set for C in quiet_components(e, k)])
-		Sr = set([(r, v) for r in node_to_face.keys() for v in G.keys()])
-
-		# print("Sr")
-		# for (r, v) in sorted(Sr):
-		# 	print(r, ":", v)
-		# print("Te")
-		# for (e, C) in sorted(Te):
-		# 	print(e, ":", sorted(C))
+		Sr = set([(r, v) for r in nodeid_to_edgeids.keys() for v in G.keys()])
 
 		losing_eC = set()
 		losing_rv = set()
 
 		for (r, v) in Sr:
-			if v in flatten(node_to_face[r]):
+			if v in flatten([edgeid_to_vertexpair[e] for e in nodeid_to_edgeids[r]]):
 				losing_rv.add((r,v))
 
 		if len(Te) == len(losing_eC) or len(Sr) == len(losing_rv):
@@ -99,14 +90,14 @@ def carving_width_rat_catching(G) -> int:
 			new_deletion = False
 
 			for (e, C) in Te:
-				if all([(edge_to_node[e], v) in losing_rv for v in C]):
+				if all([(edgeid_to_nodeid[e], v) in losing_rv for v in C]):
 					if (e, C) not in losing_eC:
 						new_deletion = True
 						losing_eC.add((e, C))
 			
 			for (e, C) in losing_eC:
-				r1 = edge_to_node[e]
-				r2 = edge_to_node[e[::-1]]
+				r1 = edgeid_to_nodeid[e]
+				r2 = edgeid_to_nodeid[-e]
 				for (r, v) in [(r1, v) for v in C] + [(r2, v) for v in C]:
 					if (r, v) not in losing_rv:
 						new_deletion = True
@@ -137,7 +128,7 @@ def carving_width_rat_catching(G) -> int:
 	
 	return binary_search_cw()
 
-G = parse_graph_to_adj()
+G = parse_text_to_adj()
 cw = carving_width_rat_catching(G)
 print("cw is", cw)
 
--- code/dual_graph.py
+++ code/dual_graph.py
@@ -4,11 +4,13 @@ from parse_graph import parse_text_to_adj, adj_to_text
 # assume not reflexive graph
 # assume clockwise or counterclockwise ordering
 def dual_graph(G: dict[int, list[int]]):
-	G2 = dict([(x, ys.copy()) for x,ys in G.items()])
 
 	# rotate neighbors such that parallel edges are consecutive
-	def rotated(l, n):
+	G2 = dict([(x, ys.copy()) for x,ys in G.items()])
+
+	def rotated(l: list[int], n: int):
 		return l[n:] + l[:n]
+
 	for x,ys in G2.items():
 		for i,y in enumerate(ys):
 			if y != ys[0]:
@@ -16,52 +18,92 @@ def dual_graph(G: dict[int, list[int]]):
 				break
 
 	# assign edge ids
-	G3 = dict([(x, ys.copy()) for x,ys in G2.items()])
-	def nth_to_last_idx_of(l, x, n):
-		return len(l) - 1 - (l[::-1].index(x) + n)
-	next_edge_id = 1
-	edges = dict()
+	# where the pair of ids, i and -i, are the two directions of the same edge
+	# and ordering is maintained
+	_G = dict([(x, ys.copy()) for x,ys in G2.items()])
+	edgeid_to_vertexpair = dict()
+
+	def opposite_idx(neighbors: list[int], nth_parallel: int, v: int):
+		return len(neighbors) - 1 - (neighbors[::-1].index(v) + nth_parallel)
+
+	next_edgeid = 1
 	for x,ys in G2.items():
-		prev = -1
-		nth = 0
+		prev = 0
+		nth_parallel = 0
 		for i,y in enumerate(ys):
 			if x > y: continue
-			if y == prev: nth += 1
-			else: nth = 0
-			G3[x][i] = next_edge_id
-			edges[next_edge_id] = (x, y)
-			G3[y][nth_to_last_idx_of(G2[y], x, nth)] = -next_edge_id
-			edges[-next_edge_id] = (y, x)
+			if y == prev: nth_parallel += 1
+			else: nth_parallel = 0
+			_G[x][i] = next_edgeid
+			edgeid_to_vertexpair[next_edgeid] = (x, y)
+			_G[y][opposite_idx(G2[y], nth_parallel, x)] = -next_edgeid
+			edgeid_to_vertexpair[-next_edgeid] = (y, x)
 			prev = y
-			next_edge_id += 1
+			next_edgeid += 1
 
-	faces = dict()
-	face_id = 1
-	unvisited = set(edges.keys())
-	edge_id_to_face_id = dict()
+	# construct dual graph
+	nodeid_to_edgeids = dict()
+	edgeids_to_nodeid = dict()
+
+	edgeid_to_nodeid = dict()
+
+	next_nodeid = 1
+	unvisited = set(edgeid_to_vertexpair.keys())
 	while len(unvisited) > 0:
 		init_edge_id = unvisited.pop()
-		face = [init_edge_id]
+		edgeids = [init_edge_id]
 		while True:
-			curr_edge_id = face[-1]
-			unvisited.discard(curr_edge_id)
-			curr_x,curr_y = edges[curr_edge_id]
-			edge_id_to_face_id[curr_edge_id] = face_id
+			curr_edgeid = edgeids[-1]
+			unvisited.discard(curr_edgeid)
+			curr_x,curr_y = edgeid_to_vertexpair[curr_edgeid]
+			edgeid_to_nodeid[curr_edgeid] = next_nodeid
 
-			idx_prev = (G3[curr_y].index(-curr_edge_id)-1)%len(G3[curr_y])
-			next_edge_id = G3[curr_y][idx_prev]
+			idx_prev = (_G[curr_y].index(-curr_edgeid)-1)%len(_G[curr_y])
+			next_edgeid = _G[curr_y][idx_prev]
 
-			if init_edge_id == next_edge_id:
-				faces[face_id] = face
-				face_id += 1
+			if init_edge_id == next_edgeid:
+				nodeid_to_edgeids[next_nodeid] = edgeids
+				edgeids_to_nodeid[frozenset(edgeids)] = next_nodeid
+				next_nodeid += 1
 				break
 
-			face.append(next_edge_id)
+			edgeids.append(next_edgeid)
 
-	dual = dict([(i, [edge_id_to_face_id[-e] for e in face]) for i,face in faces.items()])
+	_D = dict([(i, []) for i in nodeid_to_edgeids.keys()])
+	linkid_to_nodepair = dict()
+	edgeid_to_linkid = dict()
+	linkid_to_edgeid = dict()
+	next_linkid = 1
 
-	return dual
+	for e1,n1 in edgeid_to_nodeid.items():
+		if e1 < 0: continue
+		e2 = -e1
+		n2 = edgeid_to_nodeid[e2]
+		linkid_to_nodepair[next_linkid] = (n1, n2)
+		linkid_to_nodepair[-next_linkid] = (n2, n1)
+		edgeid_to_linkid[e1] = next_linkid
+		linkid_to_edgeid[next_linkid] = e1
+		edgeid_to_linkid[e2] = -next_linkid
+		linkid_to_edgeid[-next_linkid] = e2
+		_D[n1].append(next_linkid)
+		_D[n2].append(-next_linkid)
+		next_linkid += 1
 
-G = parse_text_to_adj()
-dual = dual_graph(G)
-adj_to_text(dual)
+	D = dict([(i, list(map(lambda x: linkid_to_nodepair[x][1], linkids))) for i,linkids in _D.items()])
+
+	return D, _D, linkid_to_nodepair, edgeid_to_linkid, linkid_to_edgeid, nodeid_to_edgeids, edgeid_to_nodeid, edgeid_to_vertexpair, _G, G
+
+# G = parse_text_to_adj()
+# D, _D, linkid_to_nodepair, edgeid_to_linkid, linkid_to_edgeid, nodeid_to_edgeids, edgeid_to_nodeid, G, _G, edgeid_to_vertexpair = dual_graph(G)
+# adj_to_text(D)
+
+# print("G", G)
+# print("_G", _G)
+# print("edgeid_to_vertexpair", edgeid_to_vertexpair)
+# print("D", D)
+# print("_D", _D)
+# print("linkid_to_nodepair", linkid_to_nodepair)
+# print("nodeid_to_edgeids", nodeid_to_edgeids)
+# print("edgeid_to_nodeid", edgeid_to_nodeid)
+# print("edgeid_to_linkid", edgeid_to_linkid)
+# print("linkid_to_edgeid", linkid_to_edgeid)
