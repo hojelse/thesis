1976045e25c973370ab3acbbd76142132164767b
Kristoffer Højelse <34659757+hojelse@users.noreply.github.com>
2024-05-06 12:48:37 +0200

    Update contraction to not mutate

--- code/contraction.py
+++ code/contraction.py
@@ -2,26 +2,27 @@ from parse_graph import parse_text_to_adj, adj_to_text
 
 def contraction(G: dict[int, list[int]], e: tuple[int, int]) -> dict[int, list[int]]:
 	a,b = e
+	G2 = G.copy()
+	
+	idx1 = G2[a].index(b)
+	rotated_Ga = G2[a][idx1:] + G2[a][:idx1]
 
-	idx1 = G[a].index(b)
-	rotated_Ga = G[a][idx1:] + G[a][:idx1]
+	idx2 = G2[b].index(a)
+	rotated_Gb = G2[b][idx2:] + G2[b][:idx2]
 
-	idx2 = G[b].index(a)
-	rotated_Gb = G[b][idx2:] + G[b][:idx2]
+	c = max(G2.keys()) + 1
 
-	c = max(G.keys()) + 1
+	G2[c] = rotated_Gb + rotated_Ga
 
-	G[c] = rotated_Gb + rotated_Ga
+	for x,ys in G2.items():
+		G2[x] = [(c if y==a or y==b else y) for y in ys]
 
-	for x,ys in G.items():
-		G[x] = [(c if y==a or y==b else y) for y in ys]
+	del G2[b]
+	del G2[a]
 
-	del G[b]
-	del G[a]
+	G2[c] = [y for y in G2[c] if y != a and y != b and y != c]
 
-	G[c] = [y for y in G[c] if y != a and y != b and y != c]
-
-	return G, c
+	return G2, c
 
 if __name__ == "__main__":
 	G = parse_text_to_adj()
