66c1a04d6875e377de451c18da4f625e5568ecce
Kristoffer Højelse <34659757+hojelse@users.noreply.github.com>
2024-05-06 15:44:01 +0200

    Compute branchwidth from branch-decomposition

--- code/branch_width_rat_catching.py
+++ code/branch_width_rat_catching.py
@@ -34,7 +34,6 @@ def carving_decomposition(G):
 		a,b = edges[x]
 		return (decomp(a), decomp(b))
 
-	print(edges)
 	a,b,c = G2.keys()
 	cd = (decomp(a), decomp(b), decomp(c))
 	return cd
@@ -51,10 +50,50 @@ def branch_decomposition(G):
 	bd = decomp(cd)
 	return bd
 
+def branch_width(bd):
+	t = dict()
+
+	def aux(subtree, depth, name):
+		if len(subtree) == 2 and isinstance(subtree[0], int) and isinstance(subtree[1], int):
+			t[subtree] = []
+			return subtree
+		else:
+			t[name] = []
+			for i,a in enumerate(subtree):
+				child_name = aux(a, depth+1, name+str(i))
+				t[name].append(child_name)
+				t[child_name].append(name)
+			return name
+
+	aux(bd, 0, "i0")
+
+	def leafs_set(x, y):
+		leafs = set()
+		visited = set([y])
+		stack = [x]
+		while stack:
+			v = stack.pop()
+			if isinstance(v, tuple):
+				leafs.update(set(v))
+				continue
+			if v not in visited:
+				visited.add(v)
+				for w in t[v]:
+					stack.append(w)
+		return leafs
+
+	width = 0
+	for x,ys in t.items():
+		for y in ys:
+			a = leafs_set(x, y)
+			b = leafs_set(y, x)
+			width = max(width, len(a.intersection(b)))
+	
+	return width
+
 if __name__ == "__main__":
 	G = parse_text_to_adj()
-	print(branch_decomposition(G))
-
-# edges
-# newick_format
-# newick_format -> branch_width
+	bd = branch_decomposition(G)
+	print(bd)
+	bw = branch_width(bd)
+	print(bw)
